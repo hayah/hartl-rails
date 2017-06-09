651cb2b8f8ad78d4a69340dac86d1c8734f0fb4b
Alexander A~ <suvarin@yandex.ru>
2017-06-09 21:38:58 +0300

    Add solutions to exercises

--- hello-app/app/controllers/application_controller.rb
+++ hello-app/app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: 'Hello, World!'
+    render html: '¡Hola, mundo!'
+  end
+
+  def goodbye
+    render html: 'Goodbye, world!'
   end
 end
