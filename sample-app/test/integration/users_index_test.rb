65e33babf6230468a8cd9e31c391cba3c9756510
Alexander A~ <suvarin@yandex.ru>
2017-06-24 16:33:07 +0300

    Test for the presence of both will_paginate links 10.3.4#2

--- sample-app/test/integration/users_index_test.rb
+++ sample-app/test/integration/users_index_test.rb
@@ -10,7 +10,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     p User.count
     assert_template 'users/index'
-    assert_select 'div.pagination'
+    assert_select 'div.pagination', count: 2
     User.paginate(page: 1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
