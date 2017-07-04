64b1d74211be6e3c5fab015fe3c7c94383b7797a
Alexander A~ <suvarin@yandex.ru>
2017-07-04 16:26:29 +0300

    Add micropost's association to user

--- sample-app/test/models/micropost_test.rb
+++ sample-app/test/models/micropost_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
-    @micropost = Micropost.new(content: 'Lorem lorem', user_id: @user.id)
+    @micropost = @user.microposts.build(content: 'Lorem lorem')
   end
 
   test 'should be valid' do
