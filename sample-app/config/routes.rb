40274b8702bb2f051dc893bb4224ed172424724d
Alexander A~ <suvarin@yandex.ru>
2017-07-08 21:21:22 +0300

    Add follow form and follower stats to user profile page

--- sample-app/config/routes.rb
+++ sample-app/config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     end
   end
   resources :account_activations, only: :edit
-  resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :microposts, only: [:create, :destroy]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
